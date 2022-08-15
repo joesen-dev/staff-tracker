71c33338697e4d4ee354e8d2db67a4a3c6918857
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-08-15 00:37:05 -0400

    Completed main application functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+// const db = require("./db/connection");
 const Query = require("./lib/queries");
 const Update = require("./lib/updates");
 
@@ -6,6 +7,8 @@ const {
   sqlQueries,
   sqlInsertDepartment,
   sqlInsertRole,
+  sqlInsertEmployee,
+  sqlUpdateEmployee,
 } = require("./utils/sqlQueries");
 
 const welcome = async () => {
@@ -18,10 +21,7 @@ Manager
 };
 
 // ********** Options **********
-const promptOptions = async (optionsData) => {
-  if (!optionsData) {
-    optionsData = [];
-  }
+const promptOptions = async () => {
   await inquirer
     .prompt([
       {
@@ -35,22 +35,34 @@ const promptOptions = async (optionsData) => {
           "Add Role",
           "View All Departments",
           "Add Department",
+          "Update Employee Role",
           "Quit",
         ],
       },
     ])
     .then((answersData) => {
-      optionsData.push(answersData);
       if (answersData.options === "View All Departments") {
         getDepartments();
+        setTimeout(promptOptions, 100);
       } else if (answersData.options === "View All Roles") {
         getRoles();
+        setTimeout(promptOptions, 100);
       } else if (answersData.options === "View All Employees") {
         getEmployees();
+        setTimeout(promptOptions, 100);
       } else if (answersData.options === "Add Department") {
         promptAddDepartment();
       } else if (answersData.options === "Add Role") {
         promptAddRole();
+      } else if (answersData.options === "Add Employee") {
+        promptEmployee();
+      } else if (answersData.options === "Update Employee Role") {
+        promptUpdateEmployee();
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log(error);
       }
     });
 };
@@ -59,19 +71,16 @@ const promptOptions = async (optionsData) => {
 const getDepartments = async () => {
   const printDepartments = new Query();
   sqlQueries(printDepartments.queryAllDepartments());
-  loopOptions();
 };
 // *** PRINT DEPARTMENTS
 const getRoles = async () => {
   const printRoles = new Query();
   sqlQueries(printRoles.queryAllRoles());
-  loopOptions();
 };
 // *** PRINT EMPLOYEES
 const getEmployees = async () => {
   const printEmployees = new Query();
   sqlQueries(printEmployees.queryAllEmployees());
-  loopOptions();
 };
 
 const promptAddDepartment = async () => {
@@ -87,7 +96,7 @@ const promptAddDepartment = async () => {
       console.log("Added " + answer.departmentName + " to the database");
       const addDepartment = new Update();
       sqlInsertDepartment(addDepartment.newDepartment(), answer.departmentName);
-      loopOptions();
+      setTimeout(promptOptions, 100);
     });
 };
 
@@ -153,13 +162,231 @@ const promptAddRole = async () => {
         answer.homeDepartment,
         answer.roleSalary
       );
-      loopOptions();
+      setTimeout(promptOptions, 100);
     });
 };
 
-function loopOptions() {
-  promptOptions();
-}
+const promptEmployee = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?",
+        validate: (answers) => {
+          if (answers) {
+            return true;
+          } else {
+            console.log("Please enter the employee first name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+        validate: (answers) => {
+          if (answers) {
+            return true;
+          } else {
+            console.log("Please enter the employee last name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "employeeRole",
+        message: "What is the employee's role?",
+        choices: [
+          "Sales Lead",
+          "Salesperson",
+          "Software Engineer",
+          "Account Manager",
+          "Accountant",
+          "Legal Team Lead",
+          "Lawyer",
+        ],
+        filter: (answers) => {
+          if (answers === "Sales Lead") {
+            return (answers = 1);
+          }
+          if (answers === "Salesperson") {
+            return (answers = 2);
+          }
+          if (answers === "Software Engineer") {
+            return (answers = 3);
+          }
+          if (answers === "Account Manager") {
+            return (answers = 4);
+          }
+          if (answers === "Accountant") {
+            return (answers = 5);
+          }
+          if (answers === "Legal Team Lead") {
+            return (answers = 6);
+          }
+          if (answers === "Lawyer") {
+            return (answers = 7);
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "manager",
+        message: "Who is the employee's manager?",
+        choices: [
+          "John Doe",
+          "Mike Chan",
+          "Ashley Rodriguez",
+          "Kevin Tupik",
+          "Kunal Singh",
+          "Malia Brown",
+          "Sarah Lourd",
+          "Tom Allen",
+        ],
+        filter: (answers) => {
+          console.log("L ", answers);
+          if (answers === "John Doe") {
+            return (answers = 1);
+          }
+          if (answers === "Mike Chan") {
+            return (answers = 2);
+          }
+          if (answers === "Ashley Rodriguez") {
+            return (answers = 3);
+          }
+          if (answers === "Kevin Tupik") {
+            return (answers = 4);
+          }
+          if (answers === "Kunal Singh") {
+            return (answers = 5);
+          }
+          if (answers === "Malia Brown") {
+            return (answers = 6);
+          }
+          if (answers === "Sarah Lourd") {
+            return (answers = 7);
+          }
+          if (answers === "Tom Allen") {
+            return (answers = 8);
+          }
+        },
+      },
+    ])
+    .then((answer) => {
+      console.log(
+        "Added " + answer.firstName + " " + answer.lastName + " to the database"
+      );
+      const addEmployee = new Update();
+      sqlInsertEmployee(
+        addEmployee.newEmployee(),
+        answer.firstName,
+        answer.lastName,
+        answer.employeeRole,
+        answer.manager
+      );
+      setTimeout(promptOptions, 100);
+    });
+};
+
+const promptUpdateEmployee = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employees",
+        message: "Which employee's role do you want to update?",
+        choices: [
+          "John Doe",
+          "Mike Chan",
+          "Ashley Rodriguez",
+          "Kevin Tupik",
+          "Kunal Singh",
+          "Malia Brown",
+          "Sarah Lourd",
+          "Tom Allen",
+        ],
+        filter: (answers) => {
+          console.log("L ", answers);
+          if (answers === "John Doe") {
+            return (answers = 1);
+          }
+          if (answers === "Mike Chan") {
+            return (answers = 2);
+          }
+          if (answers === "Ashley Rodriguez") {
+            return (answers = 3);
+          }
+          if (answers === "Kevin Tupik") {
+            return (answers = 4);
+          }
+          if (answers === "Kunal Singh") {
+            return (answers = 5);
+          }
+          if (answers === "Malia Brown") {
+            return (answers = 6);
+          }
+          if (answers === "Sarah Lourd") {
+            return (answers = 7);
+          }
+          if (answers === "Tom Allen") {
+            return (answers = 8);
+          }
+        },
+      },
+      {
+        type: "list",
+        name: "roles",
+        message: "Which role do you want to assign the selected employee?",
+        choices: [
+          "Sales Lead",
+          "Salesperson",
+          "Software Engineer",
+          "Account Manager",
+          "Accountant",
+          "Legal Team Lead",
+          "Lawyer",
+        ],
+        filter: (answers) => {
+          if (answers === "Sales Lead") {
+            return (answers = 1);
+          }
+          if (answers === "Salesperson") {
+            return (answers = 2);
+          }
+          if (answers === "Software Engineer") {
+            return (answers = 3);
+          }
+          if (answers === "Account Manager") {
+            return (answers = 4);
+          }
+          if (answers === "Accountant") {
+            return (answers = 5);
+          }
+          if (answers === "Legal Team Lead") {
+            return (answers = 6);
+          }
+          if (answers === "Lawyer") {
+            return (answers = 7);
+          }
+        },
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+      console.log("Updated employee's role");
+      const updateRole = new Update();
+      sqlUpdateEmployee(
+        updateRole.updateEmployee(),
+        answer.employees,
+        answer.roles,
+        answer.roleSalary
+      );
+      setTimeout(promptOptions, 100);
+    });
+};
 
 /** Start App
  ************************************************************/
--- utils/sqlQueries.js
+++ utils/sqlQueries.js
@@ -6,8 +6,7 @@ function sqlQueries(promptOptions) {
   db.promise()
     .query(promptOptions)
     .then(([rows, fields]) => {
-      const table = cTable.getTable(rows);
-      console.log(table);
+      console.table(rows);
     })
     .catch(console.log);
   // .then(() => db.end());
@@ -31,8 +30,52 @@ function sqlInsertDepartment(answer, name) {
   // .then(() => db.end());
 }
 
+// *** Add Role
 function sqlInsertRole(answer, title, department_id, salary) {
   const params = [title, department_id, salary];
+  db.promise()
+    .query(answer, params, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+    .then(([rows, fields]) => {
+      const table = cTable.getTable(rows);
+      console.log(table);
+    })
+    .catch(console.log);
+  // .then(() => db.end());
+}
+
+// *** Add Employee
+function sqlInsertEmployee(
+  answer,
+  title,
+  first_name,
+  last_name,
+  role_id,
+  manager_id
+) {
+  const params = [title, first_name, last_name, role_id, manager_id];
+  db.promise()
+    .query(answer, params, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+    .then(([rows, fields]) => {
+      const table = cTable.getTable(rows);
+      console.log(table);
+    })
+    .catch(console.log);
+  // .then(() => db.end());
+}
+
+// *** Update Employee
+function sqlUpdateEmployee(answer, employeeID, roleBeingUpdated) {
+  const params = [roleBeingUpdated, employeeID];
   db.promise()
     .query(answer, params, (err) => {
       if (err) {
@@ -52,4 +95,6 @@ module.exports = {
   sqlQueries,
   sqlInsertDepartment,
   sqlInsertRole,
+  sqlInsertEmployee,
+  sqlUpdateEmployee,
 };
